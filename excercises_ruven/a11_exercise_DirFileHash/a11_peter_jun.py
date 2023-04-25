eeba355880bae27bbf59deed7901f9bbda50d544
peet <teilzeitjobbewerber@gmx.de>
2023-04-25 09:23:53 +0200

    Exercise 11 logic finished

--- excercises_ruven/a11_exercise_DirFileHash/a11_peter_jun.py
+++ excercises_ruven/a11_exercise_DirFileHash/a11_peter_jun.py
@@ -1,38 +1,37 @@
+import hashlib, os
 
-"""
-A hash file is a file that has been converted into a numerical string
-by a mathematical algorithm.
-"""
 
 class DirFileHash:
-    def __init__(self, folder):
-        self.folder = folder
+    def __init__(self, folder_path):
+        self.folder = folder_path
+        all_files = os.listdir(folder_path)
+        # filter for only files in the directory
+        only_files = list(filter(lambda x: os.path.isfile(folder_path + "/" + x), all_files))
+        print(only_files)
 
-    ...
+        self.only_files_dict = {file_name: hashing(folder_path, file_name) for file_name in only_files}
+        print(self.only_files_dict)
 
-import hashlib
+    def __getitem__(self, file_name):
+        if file_name in self.only_files_dict:
+            return self.only_files_dict[file_name]
+        else:
+            return None
 
-file_name = 'a11_mini_file'
+def hashing(dict_path, file_name):
+    with open(dict_path + "/" + file_name) as f:
+        data = f.read()
 
-with open(file_name) as f:
-    data = f.read()
+        encode = data.encode('utf-8')
+        md5 = hashlib.md5(encode)
+        hexdigest = md5.hexdigest()
+        return hexdigest
 
-    encode = data.encode('utf-8')
-    md5 = hashlib.md5(encode)
-    hexdigest = md5.hexdigest()
+if __name__ == "__main__":
+    d = DirFileHash("a11_test_directory")
 
+    print(d["ab.py"])
 
-print("----------- data -----------")
-print(data)
-
-print("----------- encode -----------")
-print(encode)
-print("")
-print("----------- Md5 -----------")
-print(md5)
-print("")
-print("----------- hexdigest -----------")
-print(hexdigest)
 
 """
 Was muss passieren?
--- excercises_ruven/a11_exercise_DirFileHash/a11_test.py
+++ excercises_ruven/a11_exercise_DirFileHash/a11_test.py
@@ -1,4 +1,4 @@
-from solution import DirFileHash
+from a11_peter_jun import DirFileHash
 from hashlib import md5
 import pytest
 import string
